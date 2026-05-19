88e851a911ed8130d33f9497f23a66ac3fa9e048
Elisame Araújo <elisamearaujo@gmail.com>
2026-05-19 13:04:35 -0300

    feat: updated helpers.php to add HTMLHelper, PaginationHelper and RuleHelper to the list of global helpers

--- config/helpers.php
+++ config/helpers.php
@@ -7,19 +7,22 @@ return [
     | Helpers
     |--------------------------------------------------------------------------
     |
-    | Essa é uma lista de helpers personalizados para facilitar o desenvolvimento
-    | de aplicações com Laravel. Você pode adicionar ou remover helpers conforme
-    | necessário. Todos esses helpers estão disponíveis globalmente na aplicação.
+    | This is a list of custom helpers to facilitate development
+    | of applications with Laravel. You can add or remove helpers as 
+    | necessary. All of these helpers are available globally in the application.
     |
     */
 
     'global' => [
         'DateHelper'            => App\Helpers\DateHelper::class,
         'DiskHelper'            => App\Helpers\DiskHelper::class,
+        'HTMLHelper'            => App\Helpers\HTMLHelper::class,
         'MediaHelper'           => App\Helpers\MediaHelper::class,
         'NotificationHelper'    => App\Helpers\NotificationHelper::class,
         'NumberHelper'          => App\Helpers\NumberHelper::class,
+        'PaginationHelper'      => App\Helpers\PaginationHelper::class,
         'RouteHelper'           => App\Helpers\RouteHelper::class,
+        'RuleHelper'            => App\Helpers\RuleHelper::class,
         'TextHelper'            => App\Helpers\TextHelper::class,
         'UserHelper'            => App\Helpers\UserHelper::class,
     ],
